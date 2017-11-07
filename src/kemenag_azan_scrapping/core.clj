abeeaa74f92098ad6c2155710ef7ea0651106686
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-07 21:26:30 +0700

    add provinces

--- src/kemenag_azan_scrapping/core.clj
+++ src/kemenag_azan_scrapping/core.clj
@@ -1,6 +1,26 @@
-(ns kemenag-azan-scrapping.core)
+(ns kemenag-azan-scrapping.core
+  (:require [org.httpkit.client :as http]
+            [net.cgrand.enlive-html :as enl-html]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def kemenag-index (atom nil))
+
+(defn html-string->html-resource [x]
+  (enl-html/html-resource (java.io.StringReader. x)))
+
+(defn select-prov [html-resource]
+  (enl-html/select html-resource [:#opt_lokasi_provinsi]))
+
+(defn html-string->provinces [body]
+  (->> (html-string->html-resource body)
+       ((comp first select-prov))
+       (mapcat identity)
+       last
+       (filter map?)
+       (map #(:value (:attrs %)))
+       (filter #(not= "" %))))
+
+(let [{:keys [status headers body error] :as resp}
+      @(http/get "http://sihat.kemenag.go.id/waktu-sholat")]
+  (if error
+    (println "Failed, exception: " error)
+    (swap! kemenag-index (fn [_] (html-string->provinces body)))))
